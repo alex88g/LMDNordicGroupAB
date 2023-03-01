09cc764162852b7c17a00b035d1f18f698f50ae9
alex88g <alexander.gallorini@gmail.com>
2023-03-01 13:48:28 +0100

    ok

---
+++ LMDNordicGroupAB/LMDNordicGroupABApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMDNordicGroupABApp.swift
+//  LMDNordicGroupAB
+//
+//  Created by Alexander Gallorini on 2023-03-01.
+//
+
+import SwiftUI
+
+@main
+struct LMDNordicGroupABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
